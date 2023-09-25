f42fa034ec756238b1aa794f7f2f47f8c25d0b06
Haitao-Lee <hunter_lee163@163.com>
2023-09-25 22:02:42 +0800

    modified

--- UI/SRC/main.py
+++ UI/SRC/main.py
@@ -2,6 +2,9 @@
 import sys
 import vtk
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
